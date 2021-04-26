5cc2669175a429269862034d0fd9747c01757020
Carlo Grisafi <grisaficarlo@gmail.com>
2021-04-26 08:43:07 +0000

    Fixin setup script for codespace vm

--- pico_setup.sh
+++ pico_setup.sh
@@ -91,28 +91,6 @@ done
 
 cd $OUTDIR
 
-# Picoprobe and picotool
-for REPO in picoprobe picotool
-do
-    DEST="$OUTDIR/$REPO"
-    REPO_URL="${GITHUB_PREFIX}${REPO}${GITHUB_SUFFIX}"
-    git clone $REPO_URL
-
-    # Build both
-    cd $DEST
-    mkdir build
-    cd build
-    cmake ../
-    make -j$JNUM
-
-    if [[ "$REPO" == "picotool" ]]; then
-        echo "Installing picotool to /usr/local/bin/picotool"
-        sudo cp picotool /usr/local/bin/
-    fi
-
-    cd $OUTDIR
-done
-
 if [ -d openocd ]; then
     echo "openocd already exists so skipping"
     SKIP_OPENOCD=1
