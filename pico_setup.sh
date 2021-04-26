234d122bdbd16f1932f857283facc5a9bbe7004f
Carlo Grisafi <grisaficarlo@gmail.com>
2021-04-26 11:56:26 +0100

    Update pico_setup.sh

--- pico_setup.sh
+++ pico_setup.sh
@@ -4,7 +4,7 @@ git clone -b master https://github.com/raspberrypi/pico-sdk.git
 PICO_PATH="$(pwd)/pico-sdk"
 
 # Set the PICO_SDK_PATH environment variable to where you just cloned the repo.
-export PICO_SDK_PATH=$PICO_PATH
+export ${PICO_SDK_PATH}=$PICO_PATH
 
 cd pico-sdk
 git submodule update --init
