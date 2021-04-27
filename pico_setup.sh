c794908584e27fcce6a86907285f296ac2db19a9
Carlo Grisafi <grisaficarlo@gmail.com>
2021-04-27 13:10:36 +0100

    Update pico_setup.sh

--- pico_setup.sh
+++ pico_setup.sh
@@ -8,5 +8,6 @@ sudo apt update
 sudo apt install -y cmake gcc-arm-none-eabi build-essential wget
 echo "Installed dependencies"
 cd ..
+wget https://github.com/balena-io/balena-cli/releases/download/v12.44.11/balena-cli-v12.44.11-linux-x64-standalone.zip
 unzip balena-cli-v12.44.11-linux-x64-standalone.zip
 echo "Unzipped Balena release"
